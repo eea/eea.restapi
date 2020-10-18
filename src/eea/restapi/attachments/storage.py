367f37be4d40b95327484dd8032e3185ad62e2a0
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-10-18 09:18:56 +0300

    Ordered attachment storage

--- src/eea/restapi/attachments/storage.py
+++ src/eea/restapi/attachments/storage.py
@@ -1,13 +1,18 @@
+from .interfaces import IAttachmentStorage
+from .interfaces import IHasAttachments
+from .interfaces import IHasSliderImages
+from .interfaces import ISliderImagesStorage
 from persistent.mapping import PersistentMapping
+from plone.folder.ordered import CMFOrderedBTreeFolderBase
 from zope.annotation.factory import factory
 from zope.component import adapter
 from zope.interface import implementer
 
-from Acquisition import Implicit
-from OFS.Folder import Folder
 
-from .interfaces import (IAttachmentStorage, IHasAttachments, IHasSliderImages,
-                         ISliderImagesStorage)
+# from Acquisition import Implicit
+# from OFS.Folder import Folder
+# from Products.BTreeFolder2.BTreeFolder2 import BTreeFolder2
+
 
 ATTACHMENTS_KEY = 'restapi.attachments'
 SLIDER_KEY = 'slider.images.storage'
@@ -25,7 +30,8 @@ slides_annotation_storage = factory(SliderImages, key=SLIDER_KEY)
 
 @adapter(IHasAttachments)
 @implementer(IAttachmentStorage)
-class AttachmentStorage(Folder, Implicit):
+class AttachmentStorage(CMFOrderedBTreeFolderBase):     # BTreeFolder2,
+                                                        # Implicit
     """ Slider images stored in a persistent mapping
     """
 
