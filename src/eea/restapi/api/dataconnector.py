768f68c675044d905c928ad5d3373f1ca1af3e3a
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-01-25 15:31:03 +0200

    Make IDataProvider a multiadapter

--- src/eea/restapi/api/dataconnector.py
+++ src/eea/restapi/api/dataconnector.py
@@ -1,36 +1,38 @@
 # -*- coding: utf-8 -*-
-''' dataconnector '''
+""" dataconnector """
 
-import logging
 from eea.restapi.interfaces import IBasicDataProvider
 from eea.restapi.interfaces import IDataProvider
 from plone.restapi.deserializer import json_body
 from plone.restapi.interfaces import IExpandableElement
 from plone.restapi.services import Service
 from zope.component import adapter
+from zope.component import getMultiAdapter
 from zope.interface import implementer
 from zope.interface import Interface
+
+import logging
 
 
 logger = logging.getLogger()
 
 
 def handle_any(data, iov):
-    ''' data is a dictionary of lists; we need to find which indexes in those
+    """data is a dictionary of lists; we need to find which indexes in those
     lists to keep
 
     query is like  :
     [{u'i': u'NUTS_CODE',
       u'o': # u'plone.app.querystring.operation.selection.any',
-      u'v': [u'enp4do4qw8'] '''
+      u'v': [u'enp4do4qw8']"""
 
-    column = iov['i']
+    column = iov["i"]
 
     if column not in data:
         return data
 
     row = data[column]
-    value = iov['v']
+    value = iov["v"]
 
     assert isinstance(value, list)  # 'cause op below is 'in'
     indexes = [i for i, v in enumerate(row) if v in value]
@@ -40,25 +42,23 @@ def handle_any(data, iov):
     return res
 
 
-HANDLERS = {
-    u'plone.app.querystring.operation.selection.any': handle_any
-}
+HANDLERS = {u"plone.app.querystring.operation.selection.any": handle_any}
 
 
 def handle_filter(data, _filter):
-    ''' handle filter '''
-    handler = HANDLERS.get(_filter['o'])
+    """ handle filter """
+    handler = HANDLERS.get(_filter["o"])
 
     if handler is None:
-        logger.warning("Unhandled data filter %s", _filter['o'])
+        logger.warning("Unhandled data filter %s", _filter["o"])
 
     return handler(data, _filter)
 
 
 def filter_data(data, query):
-    '''this is a simple, uncomplete and limited implementation of a query
+    """this is a simple, uncomplete and limited implementation of a query
     parser.
-    See plone.app.querystring.queryparser for some details on querystrings '''
+    See plone.app.querystring.queryparser for some details on querystrings"""
 
     if not query:
         return data
@@ -72,7 +72,8 @@ def filter_data(data, query):
 @implementer(IExpandableElement)
 @adapter(IBasicDataProvider, Interface)
 class ConnectorData(object):
-    ''' connector data '''
+    """ connector data """
+
     def __init__(self, context, request):
         self.context = context
         self.request = request
@@ -87,29 +88,32 @@ class ConnectorData(object):
         if not expand:
             return result
 
-        connector = IDataProvider(self.context)
-        result['connector-data']["data"] = connector.provided_data
+        # connector = IDataProvider(self.context)
+        connector = getMultiAdapter((self.context, self.request), IDataProvider)
+        result["connector-data"]["data"] = connector.provided_data
 
         return result
 
 
 class ConnectorDataGet(Service):
-    ''' connector data - get '''
+    """ connector data - get """
+
     def reply(self):
-        ''' reply '''
+        """ reply """
         data = ConnectorData(self.context, self.request)
 
         return data(expand=True)["connector-data"]
 
 
 class ConnectorDataPost(Service):
-    ''' connector data - post '''
+    """ connector data - post """
+
     def reply(self):
-        ''' reply '''
+        """ reply """
         result = ConnectorData(self.context, self.request)(expand=True)
-        qs = json_body(self.request)['query']
+        qs = json_body(self.request)["query"]
 
-        data = filter_data(result["connector-data"]['data'], qs)
-        result['connector-data']['data'] = data
+        data = filter_data(result["connector-data"]["data"], qs)
+        result["connector-data"]["data"] = data
 
-        return result['connector-data']
+        return result["connector-data"]
--- src/eea/restapi/behavior.py
+++ src/eea/restapi/behavior.py
@@ -1,20 +1,4 @@
-''' behavior module '''
-from collections import defaultdict
-from io import StringIO
-import csv
-import logging
-import requests
-
-from plone.app.dexterity.behaviors.metadata import DCFieldProperty
-from plone.app.dexterity.behaviors.metadata import MetadataBase
-from plone.dexterity.interfaces import IDexterityContent
-from plone.memoize import ram
-from plone.rfc822.interfaces import IPrimaryFieldInfo
-from zope.component import adapter
-from zope.interface import implementer
-
-from eea.restapi.utils import timing
-
+""" behavior module """
 from .interfaces import IConnectorDataParameters
 from .interfaces import IConnectorDataProvider
 from .interfaces import IDataConnector
@@ -24,6 +8,22 @@ from .interfaces import IFacetedCollection
 from .interfaces import IFileDataProvider
 from .interfaces import IHTMLEmbed
 from .interfaces import ISimpleFacetedCollection
+from collections import defaultdict
+from eea.restapi.utils import timing
+from io import StringIO
+from plone.app.dexterity.behaviors.metadata import DCFieldProperty
+from plone.app.dexterity.behaviors.metadata import MetadataBase
+from plone.dexterity.interfaces import IDexterityContent
+from plone.memoize import ram
+from plone.rfc822.interfaces import IPrimaryFieldInfo
+from zope.component import adapter
+from zope.interface import implementer
+from zope.publisher.interfaces.browser import IBrowserRequest
+
+import csv
+import logging
+import requests
+
 
 logger = logging.getLogger(__name__)
 
@@ -31,21 +31,23 @@ logger = logging.getLogger(__name__)
 @implementer(IDataConnector)
 @adapter(IDexterityContent)
 class DataConnector(MetadataBase):
-    """ Allow data connectivity to discodata
+    """Allow data connectivity to discodata
 
     See http://discomap.eea.europa.eu/App/SqlEndpoint/Browser.aspx
     """
 
-    endpoint_url = DCFieldProperty(IDataConnector['endpoint_url'])
-    sql_query = DCFieldProperty(IDataConnector['sql_query'])
+    endpoint_url = DCFieldProperty(IDataConnector["endpoint_url"])
+    sql_query = DCFieldProperty(IDataConnector["sql_query"])
 
 
-@adapter(IConnectorDataProvider)
+@adapter(IConnectorDataProvider, IBrowserRequest)
 @implementer(IDataProvider)
 class DataProviderForConnectors(object):
-    ''' data provider for connectors '''
-    def __init__(self, context):
+    """ data provider for connectors """
+
+    def __init__(self, context, request):
         self.context = context
+        self.request = request
 
     @timing
     def _get_data(self):
@@ -53,20 +55,21 @@ class DataProviderForConnectors(object):
         # query = urllib.parse.quote_plus(self.query)
 
         try:
-            req = requests.post(self.context.endpoint_url,
-                                data={'query': self.context.sql_query})
+            req = requests.post(
+                self.context.endpoint_url, data={"query": self.context.sql_query}
+            )
             res = req.json()
         except Exception:
             logger.exception("Error in requestion data")
-            res = {'results': []}
+            res = {"results": []}
 
-        if 'errors' in res:
-            return {'results': []}
+        if "errors" in res:
+            return {"results": []}
 
         return res
 
     def change_orientation(self, data):
-        ''' change orientation '''
+        """ change orientation """
         res = {}
 
         if not data:
@@ -84,39 +87,39 @@ class DataProviderForConnectors(object):
     # TO DO: persistent caching, periodical refresh, etc
     @ram.cache(lambda func, self: self.context.modified())
     def _provided_data(self):
-        ''' provided data '''
+        """ provided data """
         if not self.context.sql_query:
             return []
 
         data = self._get_data()
 
-        return self.change_orientation(data['results'])
+        return self.change_orientation(data["results"])
 
     @property
     def provided_data(self):
-        ''' provided data '''
+        """ provided data """
         return self._provided_data()
 
 
 @implementer(IDataProvider)
-@adapter(IFileDataProvider)
+@adapter(IFileDataProvider, IBrowserRequest)
 class DataProviderForFiles(object):
-    """ Behavior implementation for content types with a File primary field
-    """
+    """Behavior implementation for content types with a File primary field"""
 
-    def __init__(self, context):
+    def __init__(self, context, request):
         self.context = context
+        self.request = request
 
     @property
     def provided_data(self):
-        ''' provided data '''
+        """ provided data """
         field = IPrimaryFieldInfo(self.context)
 
         if not field.value:
             return []
 
         text = field.value.data
-        f = StringIO(text.decode('utf-8'))
+        f = StringIO(text.decode("utf-8"))
         try:
             reader = csv.reader(f)
         except Exception:
@@ -138,38 +141,32 @@ class DataProviderForFiles(object):
 
 
 class DataVisualization(MetadataBase):
-    """ Standard Fise Metadata adaptor
-    """
+    """Standard Fise Metadata adaptor"""
 
-    visualization = DCFieldProperty(IDataVisualization['visualization'])
+    visualization = DCFieldProperty(IDataVisualization["visualization"])
 
 
 class FacetedCollection(MetadataBase):
-    """ Facetes based on indexes for collections
-    """
+    """Facetes based on indexes for collections"""
 
-    facets = DCFieldProperty(IFacetedCollection['facets'])
+    facets = DCFieldProperty(IFacetedCollection["facets"])
 
 
 class SimpleFacetedCollection(MetadataBase):
-    """ Simple Faceted Collection
-    """
+    """Simple Faceted Collection"""
 
-    filter = DCFieldProperty(ISimpleFacetedCollection['filter'])
+    filter = DCFieldProperty(ISimpleFacetedCollection["filter"])
 
 
 class HTMLEmbed(MetadataBase):
-    """ HTML Embed
-    """
+    """HTML Embed"""
 
-    embed_code = DCFieldProperty(IHTMLEmbed['embed_code'])
+    embed_code = DCFieldProperty(IHTMLEmbed["embed_code"])
 
 
 class ConnectorDataParameters(MetadataBase):
-    """ Provide predefined connector data for parameters
-    """
+    """Provide predefined connector data for parameters"""
 
     # data_parameters = DCFieldProperty(
     #     IConnectorDataParameters['data_parameters'])
-    data_query = DCFieldProperty(
-        IConnectorDataParameters['data_query'])
+    data_query = DCFieldProperty(IConnectorDataParameters["data_query"])
