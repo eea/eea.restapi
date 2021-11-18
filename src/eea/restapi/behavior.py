a711c2dcb33c224e51785a87f632aa2f45238036
Tripon Eugen <tripon.eugen@gmail.com>
2021-11-18 08:04:26 +0100

    Ref #139888 - jenkins update

--- src/eea/restapi/behavior.py
+++ src/eea/restapi/behavior.py
@@ -1,17 +1,8 @@
 """ behavior module """
-import csv
 import logging
-from collections import defaultdict
-from io import StringIO
 from plone.app.dexterity.behaviors.metadata import DCFieldProperty
 from plone.app.dexterity.behaviors.metadata import MetadataBase
-from plone.dexterity.interfaces import IDexterityContent
-from plone.rfc822.interfaces import IPrimaryFieldInfo
-from zope.component import adapter
-from zope.interface import implementer
-from zope.publisher.interfaces.browser import IBrowserRequest
 from .interfaces import IFacetedCollection
-from .interfaces import IFileDataProvider
 from .interfaces import IHTMLEmbed
 from .interfaces import ISimpleFacetedCollection
 
